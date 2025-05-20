2659cb0579c084b27b31bb0267d9a7f310aec345
Den1xx <denhis_1905@hotmail.com>
2025-05-20 21:35:11 +0300

    update

---
+++ EShopper/ViewComponents/CatTab/_ResultCatTabViewComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopper.ViewComponents.CatTab
+{
+    public class _ResultCatTabViewComponentPartial : ViewComponent
+    {
+        private IProductService _productService;
+        public _ResultCatTabViewComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var product = _productService.GetAll();
+            return View(product);
+        }
+    }
+}
---
+++ EShopper/ViewComponents/Reccommended/_ResultRecommendedViewComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopper.ViewComponents.Reccommended
+{
+    public class _ResultRecommendedViewComponentPartial : ViewComponent
+    {
+        private IProductService _productService;
+        public _ResultRecommendedViewComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var product = _productService.GetAll();
+            return View(product);
+        }
+    }
+}
