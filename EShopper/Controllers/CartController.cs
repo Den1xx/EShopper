536d02d57a73312dbee5611fc6b9c80722a93985
Den1xx <denhis_1905@hotmail.com>
2025-06-20 13:37:55 +0300

    ICartService Created

---
+++ EShopper/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using BLL.Abstract;
+using ENTITY;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopper.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CartController(ICartService cartService, IProductService productService, UserManager<ApplicationUser> userManager)
+        {
+            _cartService = cartService;
+            _productService = productService;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
