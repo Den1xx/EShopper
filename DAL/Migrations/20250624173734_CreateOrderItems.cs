5efe0d88e5f038551222dd82b2f29c01f49c4af7
Den1xx <denhis_1905@hotmail.com>
2025-06-24 20:38:22 +0300

    Order Kolonları eklendi

---
+++ DAL/Migrations/20250624173734_CreateOrderItems.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOrderItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ListPrice",
+                table: "CartItems",
+                newName: "Price");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "CartItems",
+                newName: "ListPrice");
+        }
+    }
+}
