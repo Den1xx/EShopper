97db596f65c9c283372a1c5cc1a8c160f58e2e6c
Den1xx <denhis_1905@hotmail.com>
2025-06-24 12:53:58 +0300

    CartService

--- BLL/Abstract/ICartService.cs
+++ BLL/Abstract/ICartService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.Repositories;
+using ENTITY;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace BLL.Abstract
 {
     public interface ICartService
     {
+        Task<int> AddToCartAsync(CartItem cartItem);
+        Task<Cart> GetCartByUserIdAsync(string userId);
+        void ClearCart(int cartId);
     }
 }
--- BLL/Concrete/CartService.cs
+++ BLL/Concrete/CartService.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.Abstract;
+using ENTITY;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace BLL.Concrete
 {
-    public  class CartService : ICartService
+    public class CartService : ICartService
     {
+        public Task<int> AddToCartAsync(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void ClearCart(int cartId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> GetCartByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
